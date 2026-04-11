330724c40c235a3ae6c88cd57c05e79d5b7d4a5c
akashtrinade <akash.sakhare6366@trinade.com>
2026-04-11 12:51:48 +0530

    Update site and configurations

--- trinade-new/.next/server/middleware-build-manifest.js
+++ trinade-new/.next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-2094681ed13d4f09.js","static/chunks/4bd1b696-f785427dddbba9fb.js","static/chunks/1255-29325acccda77703.js","static/chunks/main-app-6c19953d39f5d8c0.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-2094681ed13d4f09.js","static/chunks/framework-e60c938074ff7136.js","static/chunks/main-431264fe13339ba8.js","static/chunks/pages/_app-131c90850aef965b.js"],"/_error":["static/chunks/webpack-2094681ed13d4f09.js","static/chunks/framework-e60c938074ff7136.js","static/chunks/main-431264fe13339ba8.js","static/chunks/pages/_error-e4ba546eb376bdf4.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
