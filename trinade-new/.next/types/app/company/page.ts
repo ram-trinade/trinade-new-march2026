2d37f4ff3cbd8c49f7e2db14645ca278345549a3
ram-trinade <ramnarayan_kumar@trinade.com>
2026-04-14 12:37:03 +0530

    new build -14-04-26

--- trinade-new/.next/types/app/company/page.ts
+++ trinade-new/.next/types/app/company/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\aksha\OneDrive\Desktop\george\trinade-new-march2026\trinade-new\app\company\page.tsx
+// File: C:\Users\aksha\trinade-new-march2026\trinade-new\app\company\page.tsx
 import * as entry from '../../../../app/company/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
